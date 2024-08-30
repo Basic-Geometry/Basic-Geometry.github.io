7c70975ca83bd588fb5b5b2d14d90c6e1932fca2
Gaál Sándor <gmac4247@gmail.com>
2024-08-31 01:17:26 +0200

    Update site.js

--- site.js
+++ site.js
@@ -4,22 +4,25 @@ self.addEventListener('install', (event) => {
     caches.open('my-cache').then((cache) => {
       return cache.addAll([
         '/index.html',
-        '/offline.html',
-'/Images/circleToSquare1200x960.jpg',
-'/Images/circleSegment1200x960.jpg',
-'/Images/sphereVolume.jpeg',
-'/Images/sphereSlice1200x960.jpg',
-'/Images/sphereInTetra1200x960.jpg',
-'/Images/cone.jpeg',
-'/Images/frustumCone.png',
-'/Images/triangleArea1200x960.jpg',
-'/Images/conePyramidVolume.jpeg',
-'/Images/rightAngle1200x960.jpg',
-'/Images/sine1200x960.jpg',
-'/Images/equilateral1200x960.jpg',
-'/Images/regularPolygons1200x960.jpg',
-'/Images/square1200x960.jpg',
-'/Images/cube1200x960.jpg'
+        'geometry.jpeg,
+     'square.png',
+     'cubeMarkup.jpeg',
+     'trigonometry.png',
+     'pentagon.png',
+     'areaOfACircle.jpg',
+     'circleSegment.jpg',
+     'circumference.jpg',
+     'sphereAndCubeMarkup.jpeg',
+     'sphericalCap.jpg',
+     'coneAndSphereMarkup.jpeg',
+     "sphereAndCone.jpeg",
+     'eighthSphereQuarterCone.jpeg',
+     'coneAndSphere.png',
+     'coneMarkup.jpeg',
+     'frustumConeMarkup.png',
+     'conePyramidVolumeMarkup.jpeg',
+     'tetrahedronInCube.jpeg',
+        'Geometry.apk'
       ]);
     })
   );
@@ -31,7 +34,7 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).catch(() => {
         // Return the offline page if fetch fails
-        return caches.match('/offline.html');
+        return caches.match('/index.html');
       });
     })
   );
